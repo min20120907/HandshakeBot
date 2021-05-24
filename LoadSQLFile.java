e6e733ba96241dc6738a066a892b8af950d9782a
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-24 17:01:48 +0800

    add query

--- LoadSQLFile.java
+++ LoadSQLFile.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 
 public class LoadSQLFile {
     public static void main(String args[]) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please input your query: ");
+        String item = s.next();
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/HandshakeBot", "java", "password");
             Statement stmt = conn.createStatement();
             // FileReader fr = new FileReader("MilkTea.sql");
             // BufferedReader br = new BufferedReader(fr);
             // stmt.execute(br.readLine());
-            stmt.execute("SELECT * FROM MilkTea");
+            stmt.execute("SELECT * FROM MilkTea WHERE LOCATE('"+item+"',name)!=0");
             while (stmt.getResultSet().next()) {
                 System.out.print(stmt.getResultSet().getString("name") + "\t");
                 System.out.print(stmt.getResultSet().getString("class") + "\t");
