e44216cfe98b11b2565926c6a17bd202af66f89b
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-24 16:39:28 +0800

    add database reader

--- LoadSQLFile.java
+++ LoadSQLFile.java
@@ -6,13 +6,18 @@ public class LoadSQLFile {
     public static void main(String args[]) {
 
                 try {
-                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/HandshakeBot", "root", "password");
+                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/HandshakeBot", "java","password");
                     Statement stmt = conn.createStatement();
                     //FileReader fr = new FileReader("MilkTea.sql");
                     //BufferedReader br = new BufferedReader(fr);
                     //stmt.execute(br.readLine());
                     stmt.execute("SELECT * FROM MilkTea");
-                    //System.out.println(stmt.getResultSet());
+                    while(stmt.getResultSet().next()){
+                        System.out.print(stmt.getResultSet().getString("name")+"\t");
+                        System.out.print(stmt.getResultSet().getString("class")+"\t");
+                        System.out.print(stmt.getResultSet().getInt("price")+"\t");
+                        System.out.println(stmt.getResultSet().getString("brand"));
+                    }
                     stmt.close();
                     conn.close();
                 } catch (Exception e) {
