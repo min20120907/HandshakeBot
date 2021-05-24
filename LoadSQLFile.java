49f1b5236d0bfc4b04a901deb93b1813af90e1eb
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-24 16:46:46 +0800

    format

--- LoadSQLFile.java
+++ LoadSQLFile.java
@@ -1,27 +1,28 @@
 import javax.swing.*;
 import java.sql.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class LoadSQLFile {
     public static void main(String args[]) {
-
-                try {
-                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/HandshakeBot", "java","password");
-                    Statement stmt = conn.createStatement();
-                    //FileReader fr = new FileReader("MilkTea.sql");
-                    //BufferedReader br = new BufferedReader(fr);
-                    //stmt.execute(br.readLine());
-                    stmt.execute("SELECT * FROM MilkTea");
-                    while(stmt.getResultSet().next()){
-                        System.out.print(stmt.getResultSet().getString("name")+"\t");
-                        System.out.print(stmt.getResultSet().getString("class")+"\t");
-                        System.out.print(stmt.getResultSet().getInt("price")+"\t");
-                        System.out.println(stmt.getResultSet().getString("brand"));
-                    }
-                    stmt.close();
-                    conn.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/HandshakeBot", "java", "password");
+            Statement stmt = conn.createStatement();
+            // FileReader fr = new FileReader("MilkTea.sql");
+            // BufferedReader br = new BufferedReader(fr);
+            // stmt.execute(br.readLine());
+            stmt.execute("SELECT * FROM MilkTea");
+            while (stmt.getResultSet().next()) {
+                System.out.print(stmt.getResultSet().getString("name") + "\t");
+                System.out.print(stmt.getResultSet().getString("class") + "\t");
+                System.out.print(stmt.getResultSet().getInt("price") + "\t");
+                System.out.println(stmt.getResultSet().getString("brand"));
+            }
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
