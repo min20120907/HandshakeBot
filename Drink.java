99606b822dd1f4ac9dfef4406baa1629d1037de5
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-25 13:17:22 +0800

    add accessors and mutators

--- Drink.java
+++ Drink.java
@@ -1,11 +1,44 @@
-public class Drnk {
+public class Drink {
 	String name, cate, brand;
 	int price;
+
 	Drink(String name, String cate, int price, String brand) {
 		this.name = name;
 		this.cate = cate;
-		thie.price = price;
+		this.price = price;
 		this.brand = brand;
 	}
-}
 
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getCate() {
+		return cate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setCate(String cate) {
+		this.cate = cate;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
