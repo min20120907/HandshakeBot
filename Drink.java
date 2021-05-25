42f80cba17657aa758ffd54599b49d3f289a9377
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-25 13:16:21 +0800

    add Object Orienting

---
+++ Drink.java
@@ -0,0 +1,11 @@
+public class Drnk {
+	String name, cate, brand;
+	int price;
+	Drink(String name, String cate, int price, String brand) {
+		this.name = name;
+		this.cate = cate;
+		thie.price = price;
+		this.brand = brand;
+	}
+}
+
