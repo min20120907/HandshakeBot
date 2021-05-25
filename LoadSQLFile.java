88767133273881931dad8cd29ce14cc08eb2cd31
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-25 14:20:41 +0800

    update some sqlite support

--- LoadSQLFile.java
+++ LoadSQLFile.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.sql.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LoadSQLFile {
@@ -9,17 +11,19 @@ public class LoadSQLFile {
         System.out.println("Please input your query: ");
         String item = s.next();
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/HandshakeBot", "java", "password");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:HandshakeBot.db");
             Statement stmt = conn.createStatement();
             // FileReader fr = new FileReader("MilkTea.sql");
             // BufferedReader br = new BufferedReader(fr);
             // stmt.execute(br.readLine());
-            stmt.execute("SELECT * FROM MilkTea WHERE LOCATE('"+item+"',name)!=0");
+            stmt.execute("SELECT * FROM MilkTea");
+            List<Drink> drinks = new ArrayList<>();
             while (stmt.getResultSet().next()) {
-                System.out.print(stmt.getResultSet().getString("name") + "\t");
-                System.out.print(stmt.getResultSet().getString("class") + "\t");
-                System.out.print(stmt.getResultSet().getInt("price") + "\t");
-                System.out.println(stmt.getResultSet().getString("brand"));
+                String n = stmt.getResultSet().getString("name");
+                String c = stmt.getResultSet().getString("category");
+                int p = stmt.getResultSet().getInt("price");
+                String b = stmt.getResultSet().getString("brand");
+                drinks.add(new Drink(n,c,p,b));
             }
             stmt.close();
             conn.close();
